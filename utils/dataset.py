ba71dbbe5d1d0b8951858f12ab6b486bd9360052
Dominion Gbadamosi <worshipgbadamosi@live.com>
2025-06-08 01:53:32 +0100

    feat: completed functions to load cqt feature files and chord annotation files - returning PyTorch tensors

--- utils/dataset.py
+++ utils/dataset.py
@@ -1,6 +1,7 @@
 import os
 import torch
 from torch.utils.data import Dataset
+import numpy as np
 
 from utils.label_parser import load_chord_annotations, get_label_vector
 
@@ -17,3 +18,21 @@ class ChordDataset(Dataset):
         self.file_list = file_list
         self.sr = sr
         self.hop_length = hop_length
+
+    def __len__(self):
+        return len(self.file_list)
+    
+    def __getitem__(self, idx):
+        file_id = self.file_list[idx]
+        cqt_path = os.path.join(self.features_dir, file_id + ".npy")
+        lab_path = os.path.join(self.labels_dir, file_id + ".lab")
+
+        cqt = np.load(cqt_path)
+        chords = load_chord_annotations(lab_path)
+        labels = get_label_vector(chords, total_frames=cqt.shape[0],
+                                  sr=self.sr, hop_length=self.hop_length)
+        
+        cqt_tensor = torch.tensor(cqt, dtype=torch.float32)
+        label_tensor = torch.tensor(labels, dtype=torch.long)
+
+        return cqt_tensor, label_tensor
